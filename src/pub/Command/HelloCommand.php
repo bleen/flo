aa4c46fe25fac32861ad38df0c14ad567bfda1ac
Eric J. Duran <eric.duran7@gmail.com>
2014-07-25 01:23:40 -0400

    Adding composer-validate command

--- src/pub/Command/HelloCommand.php
+++ src/pub/Command/HelloCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace pub\command;
+namespace pub\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
---
+++ src/pub/Command/ValidateComposerCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ *
+ * Validate a composer.json file to have all the needed publisher settings.
+ * Ex. pub composer-validate composer.json
+ */
+
+namespace pub\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+
+class ValidateComposerCommand extends Command {
+  protected function configure() {
+    $this
+      ->setName('composer-validate')
+      ->setDescription('Validate a projects composer file for publisher & pub')
+      ->addArgument(
+        'file',
+        InputArgument::REQUIRED,
+        'composer.json file'
+      );
+
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $fs = new Filesystem();
+    $composer_file = $input->getArgument('file');
+    $errors = 0;
+
+    if ($fs->exists($composer_file)) {
+      $json = \GuzzleHttp\json_decode(file_get_contents($composer_file));
+
+      // Lets make sure our satis server is up to date.
+      if ($json->repositories[0]->url != 'http://nbcuots.github.io/satis/') {
+        $errors++;
+        $output->writeln('<error>Incorrect satis server. Correct URL Should be: http://nbcuots.github.io/satis/</error>');
+      }
+
+      // Lets make sure all install  scrpt are uptodate.
+      if ($json->scripts->{"post-install-cmd"} != 'vendor/nbcuots/publisher7/scripts/publisher-install') {
+        $errors++;
+        $output->writeln('<error>Incorrect post-install-cmd. You must have post-install-cmd set to ' .
+        '"vendor/nbcuots/publisher7/scripts/publisher-install" for publisher7.</error>');
+      }
+
+      // Lets make sure all update script are uptodate.
+      if ($json->scripts->{"post-update-cmd"} != 'vendor/nbcuots/publisher7/scripts/publisher-install') {
+        $errors++;
+        $output->writeln('<error>Incorrect post-update-cmd. You must have post-update-cmd set to ' .
+          '"vendor/nbcuots/publisher7/scripts/publisher-install" for publisher7.</error>');
+      }
+    }
+
+    if ($errors > 0) {
+      $output->writeln('Please update your composer.json. Optional: You can run pub composer-fix to fix any issues we found.');
+    }
+  }
+}
