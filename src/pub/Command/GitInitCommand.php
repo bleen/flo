cca39bb1cbf15e818deeac8860a554b244f2b6d4
Eric J. Duran <eric.duran7@gmail.com>
2014-08-01 19:52:36 -0400

    Wrap up pub git-init command

---
+++ src/pub/Command/GitInitCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace pub\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml;
+use Illuminate\Config;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Config\FileLocator;
+use pub\PHPGit\Repository;
+
+
+class GitInitCommand extends Command {
+  const GIT_REMOTE_LIST = "git remote -v | grep fetch | awk '{print $1 \"|\" $2 \";\"}'";
+
+
+  protected function configure() {
+    $this->setName('git-init')
+      ->setDescription('Initialized proper git remotes NBCUOTS & Acquia');
+    }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $fs = new Filesystem();
+    $yaml = new Yaml\Parser();
+    $profile_file = 'project-config.yml';
+    $pub_config_file = getenv("HOME") . '/.config/pub';
+
+
+    if ($fs->exists($pub_config_file)) {
+      $config = $yaml->parse($fs->get($pub_config_file));
+    }
+    else {
+      $config = array(
+        'git' => '/usr/bin/git'
+      );
+    }
+
+
+    if (!$fs->exists($profile_file)) {
+      throw new \Exception("{$profile_file} does not exists");
+    }
+
+    $value = $yaml->parse($fs->get($profile_file));
+    if (empty($value['acquia_git_uri'])) {
+      throw new \Exception("{$profile_file} is missing an acquia_git_uri property");
+    }
+
+    $git = Repository::open(getcwd(), $config['git']);
+    $remotes = $git->getCurrentRemote();
+    if (!isset($remotes['acquia']) || $remotes['acquia']['fetch'] !== $value['acquia_git_uri']) {
+      if (OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity()) {
+        $output->writeln("<info>verbose: calling git remote add acquia {$value['acquia_git_uri']}</info>>");
+      }
+
+      $git->addRemote('acquia', $value['acquia_git_uri']);
+      $output->writeln("<info>Acquia Remote added</info>");
+    }
+    else {
+      $output->writeln("<info>Acquia remote already exists</info>");
+    }
+  }
+}
---
+++ src/pub/PHPGit/Repository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace pub\PHPGit;
+
+class Repository extends \TQ\Git\Repository\Repository {
+
+
+  /**
+   * Returns the remote info
+   *
+   * This overwrite the parent method to fix a bug.
+   *
+   * @link https://github.com/teqneers/PHP-Stream-Wrapper-for-Git/pull/20
+   * @see TQ\Git\Repository\Repository::getCurrentRemote().
+   *
+   * @return  array
+   */
+  public function getCurrentRemote() {
+    /** @var $result CallResult */
+    $result = $this->getGit()->{'remote'}($this->getRepositoryPath(), array(
+      '-v'
+    ));
+    $result->assertSuccess(sprintf('Cannot remote "%s"', $this->getRepositoryPath()));
+
+    $tmp = $result->getStdOut();
+
+    preg_match_all('/([a-z]*)\h(.*)\h\((.*)\)/', $tmp, $matches);
+
+    $retVar = array();
+    foreach($matches[0] as $key => $value) {
+      $retVar[$matches[1][$key]][$matches[3][$key]] = $matches[2][$key];
+    }
+
+    return $retVar;
+  }
+
+
+  /**
+   * Add a new remote repository to a git repo.
+   *
+   * @param $name
+   * @param $uri
+   * @return array
+   */
+  public function addRemote($name, $uri) {
+    $result = $this->getGit()->{'remote'}($this->getRepositoryPath(), array(
+      'add',
+      $name,
+      $uri
+    ));
+    $result->assertSuccess(
+      sprintf('Cannot add remote for "%s"', $this->getRepositoryPath())
+    );
+
+    $output = rtrim($result->getStdOut());
+
+    return $output;
+  }
+}
+
