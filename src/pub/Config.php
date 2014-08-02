bfda2ec5be97f85cd79f2bd517ca0f8596091df8
Eric J. Duran <eric.duran7@gmail.com>
2014-08-01 21:06:01 -0400

    Adding config object

---
+++ src/pub/Config.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace pub;
+
+use Symfony\Component\Yaml;
+use Illuminate\Filesystem\Filesystem;
+
+/**
+ * Class Config
+ *
+ * Manages our ~/.config/pub file for user based configuration.
+ *
+ * @package pub
+ */
+class Config {
+
+  public $pub_config_file = '';
+
+  function __construct() {
+    $this->pub_config_file =  getenv("HOME") . '/.config/pub';
+  }
+
+  /**
+   * Returns TRUE or FALSE based on the config file existing.
+   *
+   * @return bool
+   */
+  public function exist() {
+    $retval = FALSE;
+    $fs = new Filesystem();
+
+    if ($fs->exists($this->pub_config_file)) {
+      $retval = TRUE;
+    }
+
+    return $retval;
+  }
+
+  /**
+   * Returns a loaded configuration file if it exists, FALSE otherwise.
+   * @return bool|mixed
+   */
+  public function load() {
+    $fs = new Filesystem();
+    $yaml = new Yaml\Parser();
+
+    if ($this->exist()) {
+      return FALSE;
+    }
+
+    return $yaml->parse($fs->get($this->pub_config_file));
+  }
+}
