1e28bea4da81b9ac101296337cb45da3c30b70a1
Eric J. Duran <eric.duran7@gmail.com>
2014-08-15 18:12:38 -0400

    Adding self-update command

---
+++ src/pub/Command/UpdateCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace pub\Command;
+
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateCommand extends Command {
+  const MANIFEST_FILE = 'http://nbcuots.github.io/pub/manifest.json';
+
+  protected function configure() {
+    $this->setName('self-update')
+      ->setDescription('Updates pub.phar to the latest version');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+    $manager->update($this->getApplication()->getVersion(), true);
+  }
+}
