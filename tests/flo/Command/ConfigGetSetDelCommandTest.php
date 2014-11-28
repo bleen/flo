a57c61bfc815d794cdc25fc8442c0db425020813
Eric J. Duran <eric.duran7@gmail.com>
2014-11-28 18:18:08 -0500

    Testing config-get, config-set, confi-del as a group

---
+++ tests/flo/Command/ConfigGetSetDelCommandTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace flo\Test\Command;
+
+use flo\Console\Application;
+use flo\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+
+
+
+class ConfigGetSetDelCommandTest extends \PHPUnit_Framework_TestCase {
+  /**
+   * @var  vfsStreamDirectory
+   */
+  private $root;
+
+  /**
+   * set up test environmemt filesystem.
+   */
+  public function setUp() {
+    $this->root = vfsStream::setup('home');
+    $_ENV['HOME'] = vfsStream::url('home');
+  }
+
+  /**
+   * Test the config-get, config-set, & config-del as a group.
+   */
+  public function testExecuteConfigOptions() {
+    $application = new Application();
+
+    $command_get = $application->find('config-get');
+    $command_set = $application->find('config-set');
+    $command_del = $application->find('config-del');
+
+    // flo should complain about missing config file 1st.
+    $commandTester = new CommandTester($command_get);
+    $commandTester->execute(array('command' => $command_get->getName()));
+    $this->assertContains('No flo config file exist.', $commandTester->getDisplay());
+
+    // Lets set a couple of config value. flo should confirm it saved it.
+    $commandTester = new CommandTester($command_set);
+    $commandTester->execute(array('command' => $command_set->getName(), 'config-name' => 'test1', 'config-value' => 'test1'));
+    $this->assertContains('test1: test1 has been saved.', $commandTester->getDisplay());
+
+    $commandTester = new CommandTester($command_set);
+    $commandTester->execute(array('command' => $command_set->getName(), 'config-name' => 'test2', 'config-value' => 'test2'));
+    $this->assertContains('test2: test2 has been saved.', $commandTester->getDisplay());
+
+    // Lets try to get the value via config-get now.
+    $commandTester = new CommandTester($command_get);
+    $commandTester->execute(array('command' => $command_get->getName(), 'config' => 'test1'));
+    $this->assertContains('test1: test1', $commandTester->getDisplay());
+
+    // Now lets delete the test1 key.
+    $commandTester = new CommandTester($command_del);
+    $commandTester->execute(array('command' => $command_del->getName(), 'config-name' => 'test1'));
+    $this->assertContains('test1 has been deleted.', $commandTester->getDisplay());
+
+    // Now lets try and get the test1 key.
+    $commandTester = new CommandTester($command_get);
+    $commandTester->execute(array('command' => $command_get->getName(), 'config' => 'test1'));
+    $this->assertContains("No configuration set for 'test1'", $commandTester->getDisplay());
+  }
+}
