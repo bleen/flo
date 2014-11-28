8569cea448cc42610c4df432954336365d7847ed
Eric J. Duran <eric.duran7@gmail.com>
2014-11-28 18:41:29 -0500

    Ignore our results folder

--- tests/flo/Command/ConfigGetSetDelCommandTest.php
+++ tests/flo/Command/ConfigGetSetDelCommandTest.php
@@ -39,6 +39,12 @@ class ConfigGetSetDelCommandTest extends \PHPUnit_Framework_TestCase {
     $commandTester->execute(array('command' => $command_get->getName()));
     $this->assertContains('No flo config file exist.', $commandTester->getDisplay());
 
+    // Lets try and delete a key without a config file
+    $commandTester = new CommandTester($command_del);
+    $commandTester->execute(array('command' => $command_del->getName(), 'config-name' => 'test1'));
+    $this->assertContains('No flo config file exist.', $commandTester->getDisplay());
+
+
     // Lets set a couple of config value. flo should confirm it saved it.
     $commandTester = new CommandTester($command_set);
     $commandTester->execute(array('command' => $command_set->getName(), 'config-name' => 'test1', 'config-value' => 'test1'));
@@ -58,9 +64,20 @@ class ConfigGetSetDelCommandTest extends \PHPUnit_Framework_TestCase {
     $commandTester->execute(array('command' => $command_del->getName(), 'config-name' => 'test1'));
     $this->assertContains('test1 has been deleted.', $commandTester->getDisplay());
 
+    // Now lets try and delete the same key key.
+    $commandTester = new CommandTester($command_del);
+    $commandTester->execute(array('command' => $command_del->getName(), 'config-name' => 'test1'));
+    $this->assertContains("No config key 'test1'", $commandTester->getDisplay());
+
     // Now lets try and get the test1 key.
     $commandTester = new CommandTester($command_get);
     $commandTester->execute(array('command' => $command_get->getName(), 'config' => 'test1'));
     $this->assertContains("No configuration set for 'test1'", $commandTester->getDisplay());
+
+    // Lets try to get all the value via config-get now.
+    $commandTester = new CommandTester($command_get);
+    $commandTester->execute(array('command' => $command_get->getName()));
+    $this->assertContains('test2: test2', $commandTester->getDisplay());
+
   }
 }
