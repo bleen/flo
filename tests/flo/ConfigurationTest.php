bcef43a0e57ae8dc69a36b9e87d6149d1e058525
Conor McNamara <conortm@gmail.com>
2015-03-08 15:51:27 -0400

    Added tests for Configuration class

---
+++ tests/flo/ConfigurationTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace flo\Test;
+
+use flo\Configuration;
+use flo\SymfonyOverwrite\Filesystem;
+use Symfony\Component\Process\Process;
+
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * The root project directory.
+   *
+   * @var string
+   */
+  private $root;
+
+  /**
+   * Filesystem object.
+   *
+   * @var Filesystem
+   */
+  private $fs;
+
+  /**
+   * Set up test environment filesystem.
+   */
+  public function setUp() {
+
+    $this->fs = new Filesystem();
+
+    // Attempt to create a temporary directory for the tests and change the
+    // current working directory to that directory.
+    try {
+      $this->root = sys_get_temp_dir() . '/' . str_replace('\\', '-', __CLASS__);
+      if ($this->fs->exists($this->root)) {
+        $this->fs->remove($this->root);
+      }
+      $this->fs->mkdir($this->root);
+    }
+    catch (\Exception $e) {
+      $this->tearDown();
+      // Throw the exception again so the tests will be skipped.
+      throw $e;
+    }
+    chdir($this->root);
+
+    // Setup a git repo.
+    $process = new Process('git init');
+    $process->run();
+  }
+
+  /**
+   * Test that invalid configuration throws an exception.
+   *
+   * @expectedException Exception
+   * @expectedExceptionMessageRegExp #There is an error with your configuration.*#
+   */
+  public function testInvalidConfigurationException() {
+    $valid = FALSE;
+    $this->getConfiguration($valid);
+  }
+
+  /**
+   * Test that getConfig() returns a valid array.
+   */
+  public function testGetConfig() {
+    $configuration = $this->getConfiguration();
+
+    $config = $configuration->getConfig();
+    $this->assertArrayHasKey('organization', $config);
+    $this->assertEquals('NBCUOTS', $config['organization']);
+  }
+
+  /**
+   * Test that getParameter() returns a valid value.
+   */
+  public function testGetParameter() {
+    $configuration = $this->getConfiguration();
+
+    $repository_value = $configuration->getParameter('repository');
+    $this->assertEquals('Publisher7_nbcuflo', $repository_value);
+  }
+
+  /**
+   * Test that getting an invalid parameter throws an exception.
+   *
+   * @expectedException Exception
+   * @expectedExceptionMessageRegExp #The config variable 'foo' is not set.*#
+   */
+  public function testGetParameterException() {
+    $configuration = $this->getConfiguration();
+
+    $configuration->getParameter('foo');
+  }
+
+  /**
+   * Helper function to create a flo.yml file and get a Configuration object.
+   *
+   * @param bool $valid
+   *   Whether to make the configuration file valid.
+   *
+   * @return Configuration
+   *   A Configuration object.
+   */
+  private function getConfiguration($valid = TRUE) {
+    // Create a sample flo.yml file.
+    $project_config = <<<EOT
+---
+organization: NBCUOTS
+repository: Publisher7_nbcuflo
+shortname: Publisher7_nbcuflo
+github_git_uri: git@github.com:NBCUOTS/Publisher7_nbcuflo.git
+pull_request:
+  domain: pr.publisher7.com
+  prefix: flo-test
+EOT;
+    if (!$valid) {
+      $project_config .= "\nfoo: bar";
+    }
+    $this->fs->dumpFile($this->root . "/flo.yml", $project_config);
+    return new Configuration();
+  }
+
+  /**
+   * Remove the files and directories created for this test.
+   */
+  public function tearDown() {
+    if ($this->fs->exists($this->root)) {
+      $this->fs->remove($this->root);
+    }
+  }
+
+}
